d4512c674e3e778a7149148cdd732caadd4ae740
Streds <laurenstredler@MacBook-Pro.local>
2021-06-17 20:16:01 -0500

    updates

---
+++ Lauren/app.js
@@ -0,0 +1,106 @@
+//Use d3 library to read in samples data //
+function getPlots(id) {
+    d3.json("https://covid.ourworldindata.org/data/owid-covid-data.json").then (covid_data=>{
+        console.log(covid_data)
+        var result = covid_data.samples.filter(object => object.id.toString() === id)[0];
+        console.log(result)
+        var otu_ids=result.otu_ids;
+        console.log(otu_ids)
+        var sample_values =  result.sample_values.slice(0,10).reverse();
+        console.log(sample_values)
+        //var otu_labels =  result.otu_labels.slice(0,10);
+        //console.log (otu_labels)
+        
+        // add top 10 OTUs constraint/specification //
+        var otu_top = (result.otu_ids.slice(0, 10)).reverse();
+        
+        //grab the otu data  //
+        var otu_id = otu_top.map(d => "OTU" + d);
+        console.log(`otu_ids: ${otu_id}`)
+
+        //BAR CHART //
+        var otu_labels =  result.otu_labels.slice(0,10);
+        console.log(`otu_labels: ${otu_labels}`)
+        var trace1 = {
+            type: "bar",
+            orientation: "h",
+            x: sample_values,
+            y: otu_id,
+        };
+        // data variable //
+        var data1 = [trace1];
+
+        var layout1={
+            title: "Top 10 Bacteria Cultures Found", 
+            yaxis:{
+                tickmode:"linear",
+            },
+            margin: {
+                r:75, 
+                t:55,
+                l:75,
+                b:25
+            }
+        };
+        Plotly.newPlot("bar", data1, layout1);
+
+        //NEXT CHART Bubble //
+        var trace2 = {
+            type: "bubble",
+            x: otu_ids,
+            y: sample_values,
+            mode: "markers",
+            marker: {
+                size: sample_values,
+                color: otu_ids
+            },
+            text: otu_labels
+
+        };
+        //bubble chart layout//
+        var layout2 = {
+            title: "Bacteria Cultures Per Sample",
+            xaxis:{title: "OTU ID"},
+            height: 550,
+            width: 1200,
+        };
+
+        // data variable 
+        var data2 = [trace2];
+    Plotly.newPlot("bubble", data2,layout2); 
+    
+    });
+}  
+
+function getDemoInfo(id) {
+    d3.json("samples.json").then((data)=> {
+        var metadata = data.metadata;
+        console.log(metadata)
+        var result = metadata.filter(meta => meta.id.toString() === id)[0];
+        var demoInfo = d3.select("#sample-metadata");
+        demoInfo.html(" ");
+        Object.entries(result).forEach((key) => {   
+            demoInfo.append("h6").text(key[0]+ ": " + key[1]);    
+        });
+    });
+}
+
+function optionChanged(id) {
+    getPlots(id);
+    getDemoInfo(id);
+}
+
+function init() {
+    var dropdownMenu = d3.selectAll("#selDataset");
+
+    d3.json("samples.json").then((data)=> {
+        console.log(data)
+        data.names.forEach(function(name) {
+            dropdownMenu.append("option").text(name).property("value");
+        });
+
+        getDemoInfo(data.names[0]);
+        getPlots(data.names[0]);
+    });
+}
+init();
